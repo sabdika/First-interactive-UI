66ffd01f22ed0a67ab978939aa3e77f0e67f153c
sabdika <ghimiresabdika343@gmail.com>
2021-03-13 00:01:39 +0545

    Added Count OnClick Handler

--- app/src/main/java/com/sabdika/hellotoast/MainActivity.java
+++ app/src/main/java/com/sabdika/hellotoast/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -20,5 +25,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
